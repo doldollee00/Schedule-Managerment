39686b4ab8284dbd2f26436cb3ccb970f5ebeeb3
doldollee00 <doldollee00@gmail.com>
2025-07-31 20:09:05 +0900

    feat: Lv3. 일정 수정(제목, 자작성자만 수정 가능)

--- src/main/java/org/example/memo/controller/MemoController.java
+++ src/main/java/org/example/memo/controller/MemoController.java
@@ -3,8 +3,8 @@ package org.example.memo.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.memo.dto.MemoRequestDto;
 import org.example.memo.dto.MemoResponseDto;
+import org.example.memo.entity.Memo;
 import org.example.memo.service.MemoService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,4 +38,8 @@ public class MemoController {
         return memoService.findById(id);
     }
 
+    @PatchMapping("/{id}")
+    public MemoResponseDto updateByMemo(@PathVariable Long id, @RequestBody MemoRequestDto dto) {
+        return memoService.update(id, dto.getTitle(), dto.getContent(), dto.getName(), dto.getPw());
+    }
 }
--- src/main/java/org/example/memo/service/MemoService.java
+++ src/main/java/org/example/memo/service/MemoService.java
@@ -5,9 +5,11 @@ import org.example.memo.dto.MemoRequestDto;
 import org.example.memo.dto.MemoResponseDto;
 import org.example.memo.entity.Memo;
 import org.example.memo.repository.MemoRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,7 @@ public class MemoService {
 
     private final MemoRepository memoRepository;
 
+    //일정 추가
     @Transactional
     public MemoResponseDto save(MemoRequestDto memoRequestDto) {
         //요청 받은 데이터로 객체 생성
@@ -29,6 +32,7 @@ public class MemoService {
         return new MemoResponseDto(saveMemo.getId(), saveMemo.getTitle(), saveMemo.getContent(), saveMemo.getName(), saveMemo.getCreateAt(), saveMemo.getUpdateAt());
     }
 
+    //전체 조회 및 이름 조회
     @Transactional(readOnly = true)
     public List<MemoResponseDto> findAll(String name) {
         List<Memo> memos;
@@ -52,29 +56,44 @@ public class MemoService {
         return memoResponseDtos;
     }
 
+    //단건 조회
     @Transactional(readOnly = true)
     public List<MemoResponseDto> findById(Long id) {
         //Memo의 정보를 DB에서 찾아서 Memos에 저장
-        Memo memos = memoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 " + id + " 입니다"));
+        Memo memo = memoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 " + id + " 입니다"));
         //Dto 리스트 생성 (빈 리스트)
         List<MemoResponseDto> memoResponseDtos = new ArrayList<>();
-        memoResponseDtos.add(new MemoResponseDto(memos.getId(), memos.getTitle(), memos.getContent(), memos.getName(), memos.getCreateAt(), memos.getUpdateAt()));
+        memoResponseDtos.add(new MemoResponseDto(memo.getId(), memo.getTitle(), memo.getContent(), memo.getName(), memo.getCreateAt(), memo.getUpdateAt()));
 
         return memoResponseDtos;
     }
 
+    //일정 수정
+    public MemoResponseDto update(Long id, String title, String content, String name, Long pw) {
+        //조회
+        Memo memo = memoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 " + id + " 입니다"));
+
+        // 필수값 검증
+        if (title == null || name == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The title is a required value.");
+        }
+        memo.update(title, content, name, pw);
+        Memo saveMemo = memoRepository.save(memo);
+        return new MemoResponseDto(memo.getId(), memo.getTitle(), memo.getContent(), memo.getName(), memo.getCreateAt(), memo.getUpdateAt());
+    }
 
 
 
-
-
-
+    //일정 삭제
     @Transactional()
     public ResponseEntity<Void> deleteMemo(Long id){
 
         memoRepository.deleteById(id);
         return null;
     }
+
+
 }
+
 
 
